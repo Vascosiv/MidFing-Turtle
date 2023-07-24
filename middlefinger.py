fe27431605e33c8259361c48140e312aa91450e2
Vascosiv <78032878+Vascosiv@users.noreply.github.com>
2023-07-25 06:43:28 +0800

    Add files via upload

---
+++ middlefinger.py
@@ -0,0 +1,55 @@
+'''
+by Vascosiv
+
+This is a for fun code of me in exploring turtle python.
+'''
+import turtle
+
+s = turtle.Screen()
+s.setup(width = 1.0, height = 1.0)
+t = turtle.Turtle()
+
+turtle.bgcolor("rosybrown")
+t.pensize(10)
+t.fillcolor('blue')
+t.begin_fill()
+t.lt(120)
+t.fd(70)
+t.rt(30)
+t.fd(50)
+t.rt(90)
+t.fd(30)
+t.rt(90)
+t.fd(50)
+t.bk(80)
+t.fd(0)
+t.lt(90)
+t.fd(30)
+t.rt(90)
+t.fd(80)
+t.bk(150)
+t.fd(0)
+t.lt(90)
+t.fd(30)
+t.rt(90)
+t.fd(150)
+t.bk(80)
+t.fd(0)
+t.lt(90)
+t.fd(30)
+t.rt(90)
+t.fd(80)
+t.bk(60)
+t.fd(0)
+t.lt(90)
+t.fd(30)
+t.rt(90)
+t.fd(60)
+t.rt(30)
+t.fd(70)
+t.home()
+t.end_fill()
+
+
+turtle.done()
+
